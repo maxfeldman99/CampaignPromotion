0438d3a42df4ac18507738d0dc5a28b8ff9b5d75
mfeldman <mfeldman@outbrain.com>
2024-03-21 19:40:10 +0200

    init project

---
+++ src/main/java/com/max/campaignpromotion/CampaignPromotionApplication.java
@@ -0,0 +1,13 @@
+package com.max.campaignpromotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignPromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignPromotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/max/campaignpromotion/CampaignPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.campaignpromotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignPromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
